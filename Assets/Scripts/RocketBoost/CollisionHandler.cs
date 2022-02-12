5a62b0ae9bd9b1173f6008f8c0e02b851ff707c3
Marcel Hara <MH261677@FALMOUTH.AC.UK>
2022-02-12 16:27:55 +0000

    Added collison logs

--- Assets/Scripts/RocketBoost/CollisionHandler.cs
+++ Assets/Scripts/RocketBoost/CollisionHandler.cs
@@ -4,6 +4,20 @@ public class CollisionHandler : MonoBehaviour
 {
     void OnCollisionEnter(Collision other)
     {
-        
+        switch(other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("Hit Friendly");
+                break;
+            case "Finish":
+                Debug.Log("Finished Game");
+                break;
+            case "Fuel":
+                Debug.Log("Hit Fuel");
+                break;
+            default:
+                Debug.Log("Hit object you blew up");
+                break;
+        }
     }
 }
