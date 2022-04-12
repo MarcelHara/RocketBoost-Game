e38387965c527188a7c21692fe7574c2b0cc0a6f
Marcel Hara <MH261677@FALMOUTH.AC.UK>
2022-04-12 15:47:36 +0100

    Updated BlockOscillator script and updated Scene
    Used sine wave to make the block move smoothly also lowered the roof to stop players from jumping over the top

--- Assets/Scripts/RocketBoost/BlockOscillator.cs
+++ Assets/Scripts/RocketBoost/BlockOscillator.cs
@@ -7,6 +7,7 @@ public class BlockOscillator : MonoBehaviour
     Vector3 startingPos;
     [SerializeField] Vector3 movingBlock;
     [SerializeField] [Range(0,-0.5f)] float movingFactor;  // the range box makes it a slider instead of just a number
+    [SerializeField] float period = 2f;  // period is how long lasting the sine wave will be aka 2 SECONDS DEFAULT
 
     void Start()
     {
@@ -15,7 +16,17 @@ public class BlockOscillator : MonoBehaviour
 
     private void Update()
     {
+        OscillationMovement();
         Vector3 offset = movingBlock * movingFactor;
         transform.position = startingPos + offset;
+    }
+
+    private void OscillationMovement()
+    {
+        float cycles = Time.time / period; // cycles is the currenct elapsed time x period
+
+        const float tau = Mathf.PI * 2; // tau is 2 pi, 6.28318 and is 1 complete CYCLE around the circle
+        float rawsineWave = Mathf.Sin(cycles * tau);   // cycles x tau  
+        movingFactor = (rawsineWave - 0.5f) / 2f;
     }
 }
